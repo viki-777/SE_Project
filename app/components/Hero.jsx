fd5bc2fc465b9a6b81eea5666a3871f15dd6cbb5
viki-777 <vikashkr.96453@gmail.com>
2024-10-29 22:14:20 +0530

    add OAuth with google and github

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -26,16 +26,7 @@ const Hero = () => {
       </div>
       
     </div>
-    // <div className='flex items-center gap-3 flex-col justify-center pt-14 pb-7'>
-    //     <h2 className='font-bold text-[46px] text-center'>
-    //         Find Home 
-    //         <span className='text-primary'> Service/Repair</span>
-    //         <br></br> Near You</h2>
-    //     <h2 className='text-xl text-gray-400'>Explore Best Home Service & Repair near you</h2>
-    //     <div className='mt-4 flex gap-4 items-center'>
-    //         <Search/>
-    //     </div>
-    // </div>
+  
   );
 };
 
